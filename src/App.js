315b4b9ccff593fd15e87fc488eb29a1174c0416
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-10-02 22:51:19 +0300

    webpack 4 init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { hot } from 'react-hot-loader'
+
+const App = () => {
+  return (
+    <div>
+      <p>React here!!!</p>
+	  <h1>Ted</h1>
+    </div>
+  );
+};
+
+export default hot(module)(App)
