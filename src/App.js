3d59402a66e49cf9738d16d44aa1234f9ee1d4d0
Dmitry Birin <dmitry.birin@pipedrive.com>
2018-10-02 22:58:48 +0300

    styled-components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: blue;
+`
+
+const SubTitle = styled.h3`
+	font-size: 0.8em;
+	text-align: center;
+`
 
 const App = () => {
   return (
     <div>
-      <p>React here!!!</p>
-	  <h1>Ted</h1>
+	  <Title>This is styled Title!</Title>
+	  <SubTitle>Made with React</SubTitle>
     </div>
   );
 };
