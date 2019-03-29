8f863cffec79d4b0e42c364ad088953967fd03d0
Dmitry Birin <dmitry.birin@pipedrive.com>
2019-03-29 20:47:34 +0200

    styled-icons added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
+// import { Box, Clock, Stack, Text } from 'grommet';
 import { hot } from 'react-hot-loader'
 import styled from 'styled-components'
+import { ReactLogo } from 'styled-icons/fa-brands'
 
 const Title = styled.h1`
 	font-size: 1.5em;
@@ -15,6 +17,14 @@ const Counter = styled.p`
 	font-size: 15em;
 	text-align: center;
 `
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`
+const Logo = styled(ReactLogo)`
+	color: blue;
+`
 
 const App = () => {
 	const [counter, setCounter] = useState(0)
@@ -25,11 +35,12 @@ const App = () => {
 	}, [])
 
 	return (
-		<div>
+		<Container>
 			<Title>This is styled Title!!</Title>
+			<Logo size={50} />
 			<SubTitle>Made with React</SubTitle>
 			<Counter>{counter}</Counter>
-		</div>
+		</Container>
 	)
 }
 
