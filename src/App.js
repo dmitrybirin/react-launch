225fe2c96635e184b938fe64be0a2aebfcee5a98
Dmitry Birin <dmitry.birin@pipedrive.com>
2019-12-31 16:04:58 +0200

    countdown

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
 import React, { useState, useEffect } from 'react'
-import { Clock } from 'grommet'
 import { hot } from 'react-hot-loader'
 import styled from 'styled-components'
-import { ReactLogo } from 'styled-icons/fa-brands'
 
-const Title = styled.h1`
-	font-size: 1.5em;
-	text-align: center;
-	color: blue;
-`
-const SubTitle = styled.h3`
-	font-size: 0.8em;
-	text-align: center;
-`
-const Counter = styled.p`
-	font-size: 15em;
+const Countdown = styled.p`
+	font-size: 18em;
+	font-family: 'Roboto';
 	text-align: center;
 `
 const Container = styled.div`
 	display: flex;
 	align-items: center;
+	justify-content: flex-start;
 	flex-direction: column;
-`
-const Logo = styled(ReactLogo)`
-	color: blue;
 `
 
 const App = () => {
-	const [counter, setCounter] = useState(0)
+	const [, setCounter] = useState(0)
 
 	useEffect(() => {
 		const timer = setInterval(() => setCounter(counter => counter + 1), 1000)
 		return () => clearInterval(timer)
 	}, [])
 
+	const newYearMs = +new Date('January 1, 2020 00:00:00')
+	const nowMs = +new Date()
+
+	const msLeft = newYearMs - nowMs
+
+	const secondsLeft = Math.floor(msLeft / 1000)
+	const minutesLeft = Math.floor(secondsLeft / 60)
+	const hoursLeft = Math.floor(minutesLeft / 60)
+
+	const ss = `${secondsLeft % 60}`.padStart(2, '0')
+	const mm = `${minutesLeft % 60}`.padStart(2, '0')
+	const hh = `${hoursLeft}`.padStart(2, '0')
+
 	return (
 		<Container>
-			<Clock type="digital" />
-			<Title>This is styled Title!!</Title>
-			<Logo size={50} />
-			<SubTitle>Made with React</SubTitle>
-			<Counter>{counter}</Counter>
+			<Countdown>{`${hh}:${mm}:${ss}`}</Countdown>
 		</Container>
 	)
 }
